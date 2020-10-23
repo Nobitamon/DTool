61eee390f5856191eae645c15cf73dd65cad61d9
Yize <1308269740@qq.com>
2020-10-23 09:45:59 +0800

    添加部分菜单栏、窗口最大化

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //w.showMaximized();  open and maximized the window
     w.show();
 
     return a.exec();
